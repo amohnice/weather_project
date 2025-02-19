57d9a30d29b9bc2a4a243893dc6015a3b33773fd
amohnice <amoskorir631@gmail.com>
2025-02-19 10:34:16 +0300

    added .env file to handle API keys

--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,10 @@
+import os
+
 import requests
 from django.shortcuts import render
 
 def get_weather_data(city):
-    api_key = "61fda9b8c0e2d66a412039d5da16d4b8"
+    api_key = os.getenv("OPENWEATHERMAP_API_KEY")
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
     response = requests.get(url)
     return response.json() if response.status_code == 200 else None
--- weather_project/settings.py
+++ weather_project/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env file
+
+API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
